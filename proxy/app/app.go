02e459f39298bb9041544b701f4ff24c8d8f825f
sagruvi <nedelkoaroslav@gmail.com>
2024-04-27 15:29:53 +0300

    wtf, its panic

--- proxy/app/app.go
+++ proxy/app/app.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"github.com/go-chi/chi"
-	"github.com/go-chi/jwtauth"
 	"main/internal/gateway"
 	"net/http"
 	"os"
@@ -25,7 +24,7 @@ func Run() {
 	if authport == "" {
 		authport = "authservice:15001"
 	}
-	newjwt := jwtauth.New("HS256", []byte("secret"), nil)
+	//newjwt := jwtauth.New("HS256", []byte("secret"), nil)
 	con := gateway.NewGateway(userport, geoport, authport)
 	r := chi.NewRouter()
 	r.Group(func(r chi.Router) {
@@ -33,14 +32,10 @@ func Run() {
 		r.Post("/api/auth/login", con.Login)
 	})
 	r.Group(func(r chi.Router) {
-		r.Use(jwtauth.Verifier(newjwt))
-		r.Use(jwtauth.Authenticator)
 		r.Get("/api/user/profile", con.Profile)
 		r.Get("/api/user/list", con.List)
 	})
 	r.Group(func(r chi.Router) {
-		r.Use(jwtauth.Verifier(newjwt))
-		r.Use(jwtauth.Authenticator)
 		r.Get("/api/geo/geocode", con.Geocode)
 		r.Get("/api/geo/search", con.Search)
 	})
